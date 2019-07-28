f74f18f65c338c9eb5153f4701ec52884d8d19d0
Mike Hongwei Bai <hongwei.bai@outware.com.au>
2019-07-28 18:33:38 +1000

    up test

---
+++ iosAppTestSingle/HongweiClass.swift
@@ -0,0 +1,25 @@
+//
+//  HongweiUtil.swift
+//  iosAppTestSingle
+//
+//  Created by Hongwei on 28/7/19.
+//  Copyright © 2019 hongwei. All rights reserved.
+//
+
+import Foundation
+
+class HongweiClass {
+    private var member = 44
+    
+    func getName() -> String {
+        return "Hongwei"
+    }
+    
+    func getMyLuckyNumber() -> Int {
+        return member
+    }
+}
+
+func getName() -> String {
+    return "Hongwei"
+}
--- iosAppTestSingle/ViewController.swift
+++ iosAppTestSingle/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        myLabel.text = "Hongwei Hello!"
     }
 
 
+    @IBOutlet weak var androidIcon: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    private var counter = 0
+    
+    @IBAction func myFunctionName(sender: UIButton) {
+        print(sender.tag)
+        
+//        let name = HongweiUtil
+        let hongweiObj = HongweiClass()
+        let stringFromClass = hongweiObj.getName() + "My Lucky Number is " + String(hongweiObj.getMyLuckyNumber())
+        
+        if(sender.tag == 4) {
+            counter = counter + 1
+            if(counter % 2==0) {
+                myLabel.text = "Qantas clicked!" + String(sender.tag)
+            } else {
+                myLabel.text = stringFromClass
+            }
+            
+            
+        } else {
+            myLabel.text = "Unknown Tag: " + String(sender.tag)
+        }
+    }
 }
 
