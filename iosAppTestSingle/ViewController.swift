d8f293c555e9cffd8ae6f1104923ff221b0ef24a
Mike Hongwei Bai <hongwei.bai@outware.com.au>
2019-07-28 10:48:57 +1000

    init repo ios

---
+++ iosAppTestSingle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosAppTestSingle
+//
+//  Created by Hongwei on 10/7/19.
+//  Copyright © 2019 hongwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
